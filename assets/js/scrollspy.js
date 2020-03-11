fab296f06f5b48214e35bce8667368c68adb09bd
shok7465 <savvas.papasavvas@hotmail.com>
2020-03-11 02:54:06 +0200

    v3.0

---
+++ assets/js/scrollspy.js
@@ -0,0 +1,18 @@
+$('body').scrollspy({
+  target: '#navcol-1',
+  offset: 52
+});
+
+/* smooth scrolling sections */
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 50
+        }, 1000);
+        return false;
+      }
+    }
+});
